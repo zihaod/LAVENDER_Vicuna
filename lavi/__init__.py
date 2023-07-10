e047dc25053a37034369ecf82ad6bd4b098c4305
zihaod <65971377+zihaod@users.noreply.github.com>
2023-07-10 10:58:35 -0400

    Update __init__.py

--- lavi/__init__.py
+++ lavi/__init__.py
@@ -10,22 +10,22 @@ import sys
 
 from omegaconf import OmegaConf
 
-from minigpt4.common.registry import registry
+from lavi.common.registry import registry
 
-from minigpt4.datasets.builders import *
-from minigpt4.models import *
-from minigpt4.processors import *
-from minigpt4.tasks import *
+from lavi.datasets.builders import *
+from lavi.models import *
+from lavi.processors import *
+from lavi.tasks import *
 
 
 root_dir = os.path.dirname(os.path.abspath(__file__))
-default_cfg = OmegaConf.load(os.path.join(root_dir, "configs/default.yaml"))
+#default_cfg = OmegaConf.load(os.path.join(root_dir, "configs/default.yaml"))
 
 registry.register_path("library_root", root_dir)
 repo_root = os.path.join(root_dir, "..")
 registry.register_path("repo_root", repo_root)
-cache_root = os.path.join(repo_root, default_cfg.env.cache_root)
-registry.register_path("cache_root", cache_root)
+#cache_root = os.path.join(repo_root, default_cfg.env.cache_root)
+#registry.register_path("cache_root", cache_root)
 
 registry.register("MAX_INT", sys.maxsize)
 registry.register("SPLIT_NAMES", ["train", "val", "test"])
